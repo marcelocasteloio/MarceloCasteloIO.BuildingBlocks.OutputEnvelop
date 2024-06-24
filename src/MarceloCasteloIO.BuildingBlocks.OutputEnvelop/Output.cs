16ec51341d16eeb82babd40c6c64e836422a530f
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2024-06-24 01:25:49 -0300

    feat: add output with basic overloads

---
+++ src/MarceloCasteloIO.BuildingBlocks.OutputEnvelop/Output.cs
@@ -0,0 +1,120 @@
+﻿using MarceloCasteloIO.BuildingBlocks.OutputEnvelop.Enums;
+using MarceloCasteloIO.BuildingBlocks.OutputEnvelop.Models;
+
+namespace MarceloCasteloIO.BuildingBlocks.OutputEnvelop;
+
+public readonly struct Output<TValue>
+{
+    // Constants
+    public const string OUTPUT_MESSAGE_SHOULD_BE_VALID_MESSAGE = "OutputMessage should be valid";
+    public const string EXCEPTION_SHOULD_BE_NOT_NULL_MESSAGE = "Exception should be not null";
+
+    // Properties
+    public OutputType Type { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure { get; }
+    public bool IsPartial { get; }
+
+    public TValue? Value { get; }
+    public bool HasValue { get; }
+    public bool IsSuccessAndHasValue { get; }
+
+    public OutputMessage[]? OutputMessageCollection { get; }
+    public bool HasOutputMessage { get; }
+
+    public Exception[]? ExceptionCollection { get; }
+    public bool HasException { get; }
+
+    public bool IsValid { get; }
+
+    // Constructors
+    private Output(
+        OutputType type,
+        TValue? value,
+        OutputMessage[]? outputMessageCollection,
+        Exception[]? exceptionCollection
+    )
+    {
+        Type = type;
+
+        if (type == OutputType.Success)
+            IsSuccess = true;
+        else if (type == OutputType.Failure)
+            IsFailure = true;
+        else if (type == OutputType.Partial)
+            IsPartial = true;
+
+        Value = value;
+        if (value is not null)
+        {
+            HasValue = true;
+            IsSuccessAndHasValue = type == OutputType.Success;
+        }
+
+        OutputMessageCollection = outputMessageCollection;
+        HasOutputMessage = outputMessageCollection is not null;
+
+        ExceptionCollection = exceptionCollection;
+        HasException = exceptionCollection is not null;
+
+        IsValid = true;
+    }
+
+    // Builders
+    public static Output<TValue> Create(
+        OutputType type,
+        TValue? value = default,
+        OutputMessage[]? outputMessageCollection = null,
+        Exception[]? exceptionCollection = null
+    )
+    {
+        // Validation
+        var typeValue = (short)type;
+        // Stryker disable once all
+        if (typeValue <= 0 || typeValue > 3)
+            // Stryker disable once all
+            throw new ArgumentOutOfRangeException(nameof(type));
+
+        if(outputMessageCollection is not null)
+            for (int i = 0; i < outputMessageCollection.Length; i++)
+                if (!outputMessageCollection[i].IsValid)
+                    throw new InvalidOperationException(OUTPUT_MESSAGE_SHOULD_BE_VALID_MESSAGE);
+
+        if (exceptionCollection is not null)
+            for (int i = 0; i < exceptionCollection.Length; i++)
+                if (exceptionCollection[i] is null)
+                    throw new InvalidOperationException(EXCEPTION_SHOULD_BE_NOT_NULL_MESSAGE);
+
+        // Process and return
+        return new Output<TValue>(
+            type,
+            value,
+            outputMessageCollection,
+            exceptionCollection
+        );
+    }
+    public static Output<TValue> CreateSuccess(
+        TValue? value = default,
+        OutputMessage[]? outputMessageCollection = null,
+        Exception[]? exceptionCollection = null
+    )
+    {
+        return Create(OutputType.Success, value, outputMessageCollection, exceptionCollection);
+    }
+    public static Output<TValue> CreateFailure(
+        TValue? value = default,
+        OutputMessage[]? outputMessageCollection = null,
+        Exception[]? exceptionCollection = null
+    )
+    {
+        return Create(OutputType.Failure, value, outputMessageCollection, exceptionCollection);
+    }
+    public static Output<TValue> CreatePartial(
+        TValue? value = default,
+        OutputMessage[]? outputMessageCollection = null,
+        Exception[]? exceptionCollection = null
+    )
+    {
+        return Create(OutputType.Partial, value, outputMessageCollection, exceptionCollection);
+    }
+}
---
+++ tst/UnitTests/OutputTests.cs
@@ -0,0 +1,231 @@
+﻿using FluentAssertions;
+using MarceloCasteloIO.BuildingBlocks.OutputEnvelop;
+using MarceloCasteloIO.BuildingBlocks.OutputEnvelop.Enums;
+using MarceloCasteloIO.BuildingBlocks.OutputEnvelop.Models;
+
+namespace UnitTests;
+public class OutputTests
+{
+    [Fact]
+    public void Output_Should_Created()
+    {
+        // Arrange
+        var outputTypeCollection = Enum.GetValues<OutputType>();
+        var outputCollection = new Output<string>[outputTypeCollection.Length];
+        var expectedValue = Guid.NewGuid().ToString();
+        var expectedOutputMessageCollection = new[]
+        {
+            OutputMessage.CreateInformation(code: Guid.NewGuid().ToString()),
+            OutputMessage.CreateInformation(code: Guid.NewGuid().ToString()),
+        };
+        var expectedExceptionCollection = new[]
+        {
+            new Exception(),
+            new Exception(),
+        };
+
+        // Act
+        for (int i = 0; i < outputTypeCollection.Length; i++)
+            outputCollection[i] = Output<string>.Create(
+                type: outputTypeCollection[i],
+                value: expectedValue,
+                outputMessageCollection: expectedOutputMessageCollection,
+                exceptionCollection: expectedExceptionCollection
+            );
+
+        // Assert
+        for (int i = 0; i < outputTypeCollection.Length; i++)
+        {
+            var output = outputCollection[i];
+
+            output.Type.Should().Be(outputTypeCollection[i]);
+            output.IsSuccess.Should().Be(output.Type == OutputType.Success);
+            output.IsFailure.Should().Be(output.Type == OutputType.Failure);
+            output.IsPartial.Should().Be(output.Type == OutputType.Partial);
+
+            output.Value.Should().Be(expectedValue);
+            output.HasValue.Should().BeTrue();
+
+            output.IsSuccessAndHasValue.Should().Be(output.IsSuccess && output.HasValue);
+
+            output.OutputMessageCollection.Should().BeSameAs(expectedOutputMessageCollection);
+            output.HasOutputMessage.Should().BeTrue();
+
+            output.ExceptionCollection.Should().BeSameAs(expectedExceptionCollection);
+            output.HasException.Should().BeTrue();
+
+            output.IsValid.Should().BeTrue();
+        }
+    }
+
+    [Fact]
+    public void Output_Should_Created_With_Null_Values()
+    {
+        // Arrange
+        var outputTypeCollection = Enum.GetValues<OutputType>();
+        var outputCollection = new Output<string>[outputTypeCollection.Length];
+
+        // Act
+        for (int i = 0; i < outputTypeCollection.Length; i++)
+            outputCollection[i] = Output<string>.Create(
+                type: outputTypeCollection[i]
+            );
+
+        // Assert
+        for (int i = 0; i < outputTypeCollection.Length; i++)
+        {
+            var output = outputCollection[i];
+
+            output.Type.Should().Be(outputTypeCollection[i]);
+            output.IsSuccess.Should().Be(output.Type == OutputType.Success);
+            output.IsFailure.Should().Be(output.Type == OutputType.Failure);
+            output.IsPartial.Should().Be(output.Type == OutputType.Partial);
+
+            output.Value.Should().BeNull();
+            output.HasValue.Should().BeFalse();
+
+            output.IsSuccessAndHasValue.Should().Be(output.IsSuccess && output.HasValue);
+
+            output.OutputMessageCollection.Should().BeNull();
+            output.HasOutputMessage.Should().BeFalse();
+
+            output.ExceptionCollection.Should().BeNull();
+            output.HasException.Should().BeFalse();
+
+            output.IsValid.Should().BeTrue();
+        }
+    }
+
+    [Fact]
+    public void Output_Should_Created_Success()
+    {
+        // Arrange
+        var expectedType = OutputType.Success;
+
+        // Act
+        var output = Output<string>.CreateSuccess();
+
+        // Assert
+        output.Type.Should().Be(expectedType);
+    }
+
+    [Fact]
+    public void Output_Should_Created_Failure()
+    {
+        // Arrange
+        var expectedType = OutputType.Failure;
+
+        // Act
+        var output = Output<string>.CreateFailure();
+
+        // Assert
+        output.Type.Should().Be(expectedType);
+    }
+
+    [Fact]
+    public void Output_Should_Created_Partial()
+    {
+        // Arrange
+        var expectedType = OutputType.Partial;
+
+        // Act
+        var output = Output<string>.CreatePartial();
+
+        // Assert
+        output.Type.Should().Be(expectedType);
+    }
+
+    [Fact]
+    public void Output_Should_Not_Created_With_Invalid_Type()
+    {
+        // Arrange
+        var wrongOutputTypeCollection = new OutputType[] { 
+            0,
+            (OutputType)short.MaxValue
+        };
+        var actionCollection = new Action[wrongOutputTypeCollection.Length];
+
+        // Act
+        for (int i = 0; i < wrongOutputTypeCollection.Length; i++)
+        {
+            var wrongOutputType = wrongOutputTypeCollection[i];
+            actionCollection[i] = new Action(() => Output<string?>.Create(type: wrongOutputType));
+
+        }
+
+        // Assert
+        foreach (var action in actionCollection)
+            action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void Output_Should_Not_Created_With_Invalid_OutputMessage()
+    {
+        // Arrange
+        var outputTypeCollection = Enum.GetValues<OutputType>();
+        var actionCollection = new Action[outputTypeCollection.Length];
+        var expectedValue = Guid.NewGuid().ToString();
+        var expectedOutputMessageCollection = new OutputMessage[]
+        {
+            default,
+            default,
+        };
+
+        // Act
+        for (int i = 0; i < outputTypeCollection.Length; i++)
+        {
+            var outputType = outputTypeCollection[i];
+
+            actionCollection[i] = new Action(() => 
+                Output<string>.Create(
+                    type: outputType,
+                    value: expectedValue,
+                    outputMessageCollection: expectedOutputMessageCollection
+                )
+            );
+        }
+
+        // Assert
+        foreach (var action in actionCollection)
+            action.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Output_Should_Not_Created_With_Null_Exception()
+    {
+        // Arrange
+        var outputTypeCollection = Enum.GetValues<OutputType>();
+        var actionCollection = new Action[outputTypeCollection.Length];
+        var expectedValue = Guid.NewGuid().ToString();
+        var expectedOutputMessageCollection = new OutputMessage[]
+        {
+            OutputMessage.CreateInformation(code: Guid.NewGuid().ToString()),
+            OutputMessage.CreateInformation(code: Guid.NewGuid().ToString()),
+        };
+
+        var expectedExceptionCollection = new Exception[]
+        {
+            null!,
+            null!
+        };
+
+        // Act
+        for (int i = 0; i < outputTypeCollection.Length; i++)
+        {
+            var outputType = outputTypeCollection[i];
+
+            actionCollection[i] = new Action(() =>
+                Output<string>.Create(
+                    type: outputType,
+                    value: expectedValue,
+                    outputMessageCollection: expectedOutputMessageCollection,
+                    exceptionCollection: expectedExceptionCollection
+                )
+            );
+        }
+
+        // Assert
+        foreach (var action in actionCollection)
+            action.Should().Throw<InvalidOperationException>();
+    }
+}
